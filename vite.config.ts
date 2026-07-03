0afc33c68e907e3a8941f168f3ae72f1173605cc
villanelle1522 <vickyliao1230@gmail.com>
2026-07-03 21:53:00 +0800

    讚

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
