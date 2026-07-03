9e51271742ef554cf04b2a7ccbe1bfa82fdde02f
villanelle1522 <vickyliao1230@gmail.com>
2026-07-04 00:34:24 +0800

    讚

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(() => {
   return {
     base: './',
+    define: {
+      'process.env': {},
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
